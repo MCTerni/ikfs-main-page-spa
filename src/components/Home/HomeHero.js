553e5c7b711c6b94daf0de1c99221fdd98d844e0
Mateus Carnevalli Terni <mcterni@yahoo.com>
2021-01-03 12:34:25 -0500

    Carousel Done

--- src/components/Home/HomeHero.js
+++ src/components/Home/HomeHero.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css'
+import 'owl.carousel/dist/assets/owl.theme.default.css'
+import '../../css/custom.css'
 
-import imgHero1 from "../../img/hero/hero-1.jpg"
-import imgHero2 from "../../img/hero/hero-2.jpg"
+import imgHero1 from "../../img/hero/hero-1.jpg";
+import imgHero2 from "../../img/hero/hero-2.jpg";
 
 function HomeHero() {
     return (
         <div>
             {/* Hero Section Begin */}
             <section className="hero-section">
-                <div className="hs-slider owl-carousel">
-                    <div className="hs-item set-bg" data-setbg={imgHero1} >
+                <OwlCarousel className="hs-slider owl-carousel owl-theme owl-nav" items={1}
+                loop
+                nav
+                margin={0}
+                >
+                    <div className="hs-item " style={{backgroundImage:`url(${imgHero1})`}}>
                         <div className="container">
                             <div className="row">
                                 <div className="col-lg-6 offset-lg-6">
@@ -22,7 +30,7 @@ function HomeHero() {
                             </div>
                         </div>
                     </div>
-                    <div className="hs-item set-bg" data-setbg={imgHero2}>
+                    <div className="hs-item set-bg" style={{backgroundImage:`url(${imgHero2})`}}>
                         <div className="container">
                             <div className="row">
                                 <div className="col-lg-6 offset-lg-6">
@@ -35,7 +43,7 @@ function HomeHero() {
                             </div>
                         </div>
                     </div>
-                </div>
+                </OwlCarousel>
             </section>
             {/* Hero Section End */}
         </div>
